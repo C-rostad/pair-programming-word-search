bdb1feaaf925ad1f7dc70097e2ed3f1864e0d1c6
C-rostad <Carter@rostad.ca>
2025-06-14 13:00:11 -0700

    First implementation of wordsearch, completed with Daniela Pachon

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,34 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  it("should return false if the array is empty", function () {
+    const emptyArray = [];
+    const result = wordSearch(emptyArray, "WORD");
+    assert.equal(result, false);
+  });
+  it("should return false if there is no word input", function () {
+    const result = wordSearch(["1","2","3","4"], "");
+    assert.equal(result, false);
+  });
+  it("should return false if the word input is not a string", function() {
+    const result = wordSearch(["1","2","3","4"], 2);
+    assert.equal(result, false);
+  });
+  it("should return false if the first input is not an array", function() {
+    const result = wordSearch("NOT ARRAY TEST", "ARRAY");
+    assert.equal(result, false);
+  });
+  it("should return true if the word is in the array vertically", function() {
+    const result = wordSearch([
+  ['A', 'S', 'Y','H', 'W', 'B','U', 'O', 'E'],
+  ['W', 'E', 'F','M', 'H', 'F','B', 'D', 'Z'],
+  ['C', 'I', 'C','J', 'C', 'R','T', 'C', 'K'],
+  ['F', 'N', 'F','T', 'S', 'E','W', 'A', 'F'],
+  ['Q', 'F', 'Q','E', 'Y', 'N','A', 'K', 'Q'],
+  ['U', 'E', 'U','V', 'E', 'E','P', 'U', 'U'],
+  ['A', 'L', 'A','R', 'R', 'Y','A', 'A', 'A'],
+  ['L', 'D', 'L','G', 'L', 'B','I', 'S', 'L']], `SEINFELD`);
+    assert.equal(result,true);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,41 @@
+//Week 5 Pair Programming by Carter Rostad https://github.com/C-rostad and Daniela Pachon
+
+const transpose = function(matrix) {
+  const result = [];
+  if (!matrix) { //check if input is valid
+    return result;
+  }
+
+  for (let col = 0; col < matrix[0].length; col++) { // first loop
+    const newRow = [];
+
+    for (let row = 0; row < matrix.length; row++) { // second loop
+      newRow.push(matrix[row][col]);
+    
+    } //second loop end
+    result.push(newRow);
+  } //first loop end
+  return result;
+};
+
+
 const wordSearch = (letters, word) => { 
+    if (!word || !letters || typeof word !== "string" || !Array.isArray(letters) || letters.length === 0) {
+        return false;
+    }
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
     const horizontalJoin = letters.map(ls => ls.join(''))
+   
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+
+    for (l of verticalJoin) {
+    if (l.includes(word)) return true
+    }
+    return false;
+    
 }
 
 module.exports = wordSearch
